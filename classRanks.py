dc97334c65c8aa07b1fb3d22564ed223dacf5e3e
AnnabelFlook <annabel.flook@symetrics.co.uk>
2021-05-19 10:06:37 +0100

    Code tidied

--- classRanks.py
+++ classRanks.py
@@ -1,5 +1,4 @@
 import main
-from sklearn import preprocessing
 
 
 class Rank:
@@ -10,18 +9,14 @@ class Rank:
 
     def find_best_modules(self, merged_df):
         """Find the best modules for these ranks"""
+        merged_df = merged_df.loc[:, self.syllabus]
         filtered_df = main.filter_data(merged_df, self.ranks)
+
         date_matrix = main.create_date_matrix(filtered_df)
-
-        minmax_scaler = preprocessing.MinMaxScaler(feature_range=(0.1, 10))
-
         cdt_vector = main.create_cdt_vector(date_matrix)
-        scaled_cdt_vector = minmax_scaler.fit_transform(cdt_vector.T)  # 20 x 1 vector
-
         mod_vector = main.create_module_vector(date_matrix)
-        scaled_mod_vector = minmax_scaler.fit_transform(mod_vector.T)  # 48 x 1 vector
 
-        ratings = main.create_rating_matrix(cdt_vec=scaled_cdt_vector, mod_vec=scaled_mod_vector, date_mat=date_matrix)
+        ratings = main.create_rating_matrix(cdt_vec=cdt_vector, mod_vec=mod_vector, date_mat=date_matrix)
 
         best_modules = ratings.sum(axis=0)
         best_modules_sorted = best_modules.replace(0, (best_modules.max() + 1)).sort_values(ascending=False)
@@ -30,9 +25,7 @@ class Rank:
 
     def show_best_modules(self, merged_df, n=5):
 
-        merged_df = merged_df.loc[:, self.syllabus]
         best_modules_sorted = self.find_best_modules(merged_df)
-
         print(best_modules_sorted.head(n))
 
         return best_modules_sorted
--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-from os import scandir, getcwd
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 from ExtractData import extract_data
 
-
-cdt_csv = pd.read_csv(r'C:\Users\AnnabelFlook\PycharmProjects\CadetsTP\cadet_training_program_matrix.csv')
-minmax_scaler = preprocessing.MinMaxScaler(feature_range=(0.1, 10))
-
-
-def create_df():
-    """Merge all .csv files in folder
-    :return merged pd.DataFrame"""
-    cwd = getcwd()
-    training_path = cwd + r'\TrainingData'
-    print(training_path)
-    filenames = scandir(training_path)
-    dfs = [pd.read_csv(csv, index_col=0) for csv in filenames]
-
-    join_df = pd.concat(dfs, axis=1, join='inner')
-    join_df['Rank'] = join_df.index.str[-19:-16].str.strip().str.replace('1st', 'Cdt 1st')
-    join_df = join_df.drop(['Unnamed: 1', 'Unnamed: 2'], axis=1)
-
-    return join_df
 
 
 def filter_data(merged_df, ranks: list, syllabus='all'):
@@ -54,12 +34,13 @@ def create_cdt_vector(date_mat):
     cdt_df = date_mat.apply(sum, axis=1)
     cdt_vec = np.array([cdt_df])
 
+    minmax_scaler = preprocessing.MinMaxScaler(feature_range=(0.1, 10))
     scaled_cdt_vector = minmax_scaler.fit_transform(cdt_vec.T)  # 20 x 1 vector
 
     assert cdt_vec.shape == (1, len(date_mat.index))
     assert scaled_cdt_vector.shape == (len(date_mat.index), 1)
 
-    return cdt_vec
+    return scaled_cdt_vector
 
 
 def create_module_vector(date_mat):
@@ -69,12 +50,14 @@ def create_module_vector(date_mat):
     assert sum(mod_df) < np.inf
 
     mod_vec = np.array([mod_df])
-    scaled_cdt_vector = minmax_scaler.fit_transform(mod_vec.T)
+
+    minmax_scaler = preprocessing.MinMaxScaler(feature_range=(0.1, 10))
+    scaled_mod_vector = minmax_scaler.fit_transform(mod_vec.T)
 
     assert mod_vec.shape == (1, len(date_mat.columns))
-    assert scaled_cdt_vector.shape == (len(date_mat.columns), 1)
+    assert scaled_mod_vector.shape == (len(date_mat.columns), 1)
 
-    return mod_vec
+    return scaled_mod_vector
 
 
 def create_rating_matrix(cdt_vec, mod_vec, date_mat):
@@ -86,27 +69,20 @@ def create_rating_matrix(cdt_vec, mod_vec, date_mat):
     assert cdt_mod_mat.shape == (len(date_mat.index), len(date_mat.columns))
 
     rating_mat = date_mat * cdt_mod_mat
-    # print(rating_mat)
     return rating_mat
 
 
 if __name__ == '__main__':
-    #create_df()
     cdt_csv = extract_data()
-
 
     rank_list = ['Cdt']
     filtered_df = filter_data(cdt_csv, rank_list)
 
     date_matrix = create_date_matrix(filtered_df)
-
     cdt_vector = create_cdt_vector(date_matrix)
-    scaled_cdt_vector = minmax_scaler.fit_transform(cdt_vector.T)  # 20 x 1 vector
-
     mod_vector = create_module_vector(date_matrix)
-    scaled_mod_vector = minmax_scaler.fit_transform(mod_vector.T)  # 48 x 1 vector
 
-    ratings = create_rating_matrix(cdt_vec=scaled_cdt_vector, mod_vec=scaled_mod_vector, date_mat=date_matrix)
+    ratings = create_rating_matrix(cdt_vec=cdt_vector, mod_vec=mod_vector, date_mat=date_matrix)
 
     best_modules_sorted = ratings.sum(axis=0).sort_values(ascending=False)
     print(best_modules_sorted.head(4))
